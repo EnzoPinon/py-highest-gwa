04239ee58b9b67c0c9f8e8d2074e19b85345a933
EnzoPinon <mrlorenzopinon@gmail.com>
2023-04-26 20:40:50 +0800

    collect all floats but have yet to compare each

--- highest.py
+++ highest.py
@@ -4,10 +4,15 @@ def process():
     with open("student_list.txt", 'r') as my_file:        
         #split the line into a list where we can fetch the GWA.
         for line in my_file:
-            new_list = line.split()     
-            # search for the GWA in the list.
-            for item in new_list:
-                # conditional statement.
-                print(item.isdigit())
+           #split it and turn it into a list.
+           new_list = line.split()
+           #make a new loop for every list that comes out.
+           for item in new_list:
+               # attempt to float all items, ignore if fail:
+               try:
+                new_number = float(item)
+                print(new_number)
+               except:
+                   pass
 
 process()
