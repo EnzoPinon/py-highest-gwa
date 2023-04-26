13f5de5b49862339693bc87ecd153cf78448779a
EnzoPinon <mrlorenzopinon@gmail.com>
2023-04-26 23:34:01 +0800

    fixed comparison logic and update student list

--- highest.py
+++ highest.py
@@ -1,19 +1,24 @@
 # getting the student list.
 
 def process():
+    #initialize the old number as 6.0 so the loop will update it at any time.
+    old_number = 6.0
     with open("student_list.txt", 'r') as my_file:        
-        #split the line into a list where we can fetch the GWA.
+        #Read the line one by one
         for line in my_file:
-           #split it and turn it into a list.
+           #split each and turn it into a list.
            new_list = line.split()
+           print(new_list)
            #make a new loop for every list that comes out.
            for item in new_list:
+               print(item)
                # attempt to float all items, ignore if fail:
                try:
                  new_number = float(item)
-                 old_number = 6.0
+                 #compare old and new number
                  if old_number > new_number:
                      old_number = new_number
-               except:
-                   pass
+               except ValueError:
+                   continue
+
 process()
