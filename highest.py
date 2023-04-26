54ad8852ba78137ea67328b103f9ff58d786ebf0
EnzoPinon <mrlorenzopinon@gmail.com>
2023-04-26 19:42:58 +0800

    forgot to call the function, my bad

--- highest.py
+++ highest.py
@@ -1,9 +1,11 @@
 # getting the student list.
 
 def process():
-    with open('student_list.txxt') as my_file:
+    with open("student_list.txt", 'r') as my_file:
         
         #split the line into a list where we can fetch the GWA.
         for line in my_file:
             new_list = line.split()
             print(new_list)
+
+process()
