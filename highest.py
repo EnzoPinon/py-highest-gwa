6392bc3b0b4f01800ceab87beda01d1a8c2bd618
EnzoPinon <mrlorenzopinon@gmail.com>
2023-04-26 19:52:32 +0800

    looking for the GWA in a list.

--- highest.py
+++ highest.py
@@ -1,11 +1,13 @@
 # getting the student list.
 
 def process():
-    with open("student_list.txt", 'r') as my_file:
-        
+    with open("student_list.txt", 'r') as my_file:        
         #split the line into a list where we can fetch the GWA.
         for line in my_file:
-            new_list = line.split()
-            print(new_list)
+            new_list = line.split()     
+            # search for the GWA in the list.
+            for item in new_list:
+                # conditional statement.
+                print(item.isdigit())
 
 process()
